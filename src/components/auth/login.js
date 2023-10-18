500100fbd4b62c8b67874c0c30041a4784bb3617
ranjan66 <ranjan66@gmail.com>
2023-10-18 00:45:08 -0500

    ant d form add

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,8 +1,106 @@
 import React from 'react';
-
+import {Row, Col, Button, Checkbox, Form, Input, Typography, notification, Alert} from 'antd';
+import './css/login.css'
+const {Title, Text, Link} = Typography
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
 function Login(props) {
     return (
-        <div>Login</div>
+        <>
+            <Row align="middle">
+                <Col className="gutter-row" span={10} offset={2}>
+                    <div className='main-form'>
+                        <div className='form-header'>
+                            <Title style={{textAlign: "center", fontWeight: 700,
+                                fontSize: '30px',
+                                width: '295px',
+                                height:' 42px',
+                                lineHeight: '42px'}}>
+                                Login to Continue
+                            </Title>
+                            <Text type="secondary" style={{fontWeight: 300,
+                                fontSize: '16px',
+                                textAlign: "right"
+                            }}>
+                                Welcome please enter your email address
+                            </Text>
+
+                        </div>
+
+                        <div className='login-form'>
+                            <Form
+                                layout={"vertical"}
+                                name="basic"
+
+                                /*onFinish={}
+                                onFinishFailed={}*/
+                                autoComplete="off"
+                            >
+                                <Form.Item
+                                    label="Email"
+                                    style={{
+                                        width: '410px',
+                                        marginBottom: '30px',
+                                        height: '50px',
+                                        fontWeight: 600}}
+                                    name="email"
+                                    rules={[
+                                        {
+                                            required: false,
+                                            message: 'Please input your Login ID!',
+                                        },
+                                    ]}
+                                >
+                                    <Input placeholder="Ex: email id" />
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Password"
+                                    name="password"
+                                    style={{
+                                        width: '410px',
+                                        height: '50px',
+                                        marginBottom: '40px',
+                                        fontWeight: 600}}
+                                    rules={[
+                                        {
+                                            required: false,
+                                            message: 'Please input your password!',
+                                        },
+                                    ]}
+                                >
+                                    <Input.Password placeholder="Min. 8 characters" />
+                                </Form.Item>
+
+
+                                <Form.Item>
+                                    <Button type="primary" block htmlType='submit'>
+                                        Login
+                                    </Button>
+                                </Form.Item>
+                                <Form.Item  wrapperCol={{
+                                    offset: 8,
+                                    span: 16,
+                                }}>
+                                    <a className="login-form-forgot" href="">
+                                        Forgot password
+                                    </a>
+                                </Form.Item>
+
+
+                            </Form>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="gutter-row" span={12} >
+                    <div className="aside-image">
+                    </div>
+                </Col>
+            </Row>
+
+        </>
     );
 }
 
--- src/components/auth/registration.js
+++ src/components/auth/registration.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AutoComplete, Button, Cascader, Checkbox, Col, Form, Input, InputNumber, Row, Select} from "antd";
+import './css/registration.css'
 
 function Registration(props) {
+    const [form] = Form.useForm();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        form.validateFields((err, values) => {
+            if (!err) {
+                // Submit the form data to your backend server
+            }
+        });
+    };
     return (
-        <div>Rgistration</div>
+        <>
+            <Row></Row>
+            <Row justify="center">
+                <Form layout="horizontal">
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please enter a username.' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please enter your email address.' }, { type: 'email', message: 'Please enter a valid email address.' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please enter a password.' }, { min: 6, message: 'Password must be at least 6 characters long.' }]}>
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item label="Confirm Password" name="confirm_password" rules={[{ required: true, message: 'Please confirm your password.' }, { validator: (rule, value) => { if (value !== this.getFieldValue('password')) { return Promise.reject('Passwords must match.'); } return Promise.resolve(); }}]}>
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">Register</Button>
+                    </Form.Item>
+                </Form>
+            </Row>
+        </>
+
+
+
     );
 }
 
