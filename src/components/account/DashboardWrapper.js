e0153b65715bf68bc1d2ced0d8122ad87558d440
Longbowou <blandedaniel@gmail.com>
2023-10-19 11:17:04 -0500

    Add dynamic stats and redesign Dashboard
    The commit brings a significant change to the DashboardPage by implementing useEffect to dynamically fetch and display key statistics. Also, added 'KTIcon' for illustrative purposes. The layout of the page is restructured to make it shared resource friendly and to improve user experience. The changes aim to render real-time data and enhance navigability.

--- src/components/account/DashboardWrapper.js
+++ src/components/account/DashboardWrapper.js
@@ -1,29 +1,48 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {PageTitle} from "../../_metronic/layout/core";
+import {KTIcon} from "../../_metronic/helpers";
+import {useEffect} from "react";
 
-const DashboardPage = () => (
-    <>
+const DashboardPage = () => {
+    useEffect(() => {
+        const data = ["1 000$", '5']
+        document.querySelectorAll(".fetch-stats")
+            .forEach((el, index) => {
+                setTimeout(() => {
+                    el.textContent = data[index];
+                }, 700)
+            })
+    }, []);
+    return <>
         {/* begin::Row */}
-        <div className='row g-5 g-xl-10 mb-5 mb-xl-10'>
-            {/* begin::Col */}
-            <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10'>
-
+        <div className='row g-5 g-xl-10 mb-5 mb-xl-10 justify-content-center'>
+            <div className="col-sm-12 col-md-3" data-kt-indicator="on">
+                <a href="#" className="card bg-body hoverable card-xl-stretch mb-xl-8">
+                    <div className="card-body p-5 text-center">
+                        <KTIcon iconType="duotone" iconName="abstract-23" className="fs-3x"/>
+                        <div id="requests_count" className="fetch-stats text-gray-900 fw-bolder fs-2 mb-2 mt-5">
+                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                        </div>
+                        <div className="fw-bold text-gray-400">Earnings</div>
+                    </div>
+                </a>
             </div>
-            {/* end::Col */}
 
-            {/* begin::Col */}
-            <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10'>
+            <div className="col-sm-12 col-md-3" data-kt-indicator="on">
+                <a href="#" className="card bg-body hoverable card-xl-stretch mb-xl-8">
+                    <div className="card-body p-5 text-center">
+                        <KTIcon iconType="duotone" iconName="crown" className="fs-3x"/>
+                        <div id="requests_count" className="fetch-stats text-gray-900 fw-bolder fs-2 mb-2 mt-5">
+                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                        </div>
+                        <div className="fw-bold text-gray-400">Products</div>
+                    </div>
+                </a>
             </div>
-            {/* end::Col */}
-
-            {/* begin::Col */}
-            <div className='col-xxl-6'>
-            </div>
-            {/* end::Col */}
         </div>
         {/* end::Row */}
     </>
-)
+}
 
 const DashboardWrapper = () => {
     return (
