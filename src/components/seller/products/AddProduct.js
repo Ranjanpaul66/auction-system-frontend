d42dede2da37d83ee59f668334b330050b16b709
Longbowou <blandedaniel@gmail.com>
2023-10-21 11:42:06 -0500

    Optimize form structure in AddProduct component
    This commit improves the layout of the form in the AddProduct.js file by organizing the form controls in a more intuitive manner. Form inputs and their corresponding labels have been refactored for improved readability and a more user-friendly interface. The className attributes were changed and more appropriate attributes such as 'rows' for textarea were implemented. The layout changes are intended to create an easier and more pleasing user experience when adding products.

--- src/components/seller/products/AddProduct.js
+++ src/components/seller/products/AddProduct.js
@@ -21,26 +21,27 @@ const AddProductPage = () => {
                 <div className="row mb-5 justify-content-center">
                     <div className="col-sm-12 col-lg-6">
                         <form action="" method="post" id="form">
-                            <div className="form-floating mb-7">
+                            <div className="col-md-12 fv-row mb-5">
+                                <label className="fs-5 fw-bold mb-2" htmlFor="name">Name</label>
                                 <input type="text" className="form-control" id="name" placeholder="Name"/>
-                                <label htmlFor="name">Name</label>
                             </div>
 
-                            <div className="form-floating mb-7">
+                            <div className="col-md-12 fv-row mb-5">
+                                <label className="fs-5 fw-bold mb-2" htmlFor="description">Description</label>
                                 <textarea id="description" className="form-control"
-                                          placeholder="Description" style={{height: '140px'}}></textarea>
-                                <label htmlFor="description">Description</label>
+                                          placeholder="Description" rows='4'></textarea>
                             </div>
 
-                            <div className="form-floating">
+                            <div className="col-md-12 fv-row mb-5">
+                                <label className="fs-5 fw-bold mb-2" htmlFor="description">Categories</label>
                                 <select multiple className="form-select" id="floatingSelect"
                                         aria-label="Floating label select example" style={{height: '140px'}}>
                                     <option value="1">One</option>
                                     <option value="2">Two</option>
                                     <option value="3">Three</option>
                                 </select>
-                                <label htmlFor="floatingSelect">Categories</label>
                             </div>
+
                             <button type="submit" className="btn btn-light-dark mt-3">
                                 Submit
                             </button>
