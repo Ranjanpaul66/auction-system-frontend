3de7b94f3b26fe29c81ace8d00b7037167e79e35
Longbowou <blandedaniel@gmail.com>
2023-10-22 01:27:27 -0500

    Add animated value counter to the SellerDashboard
    Incorporated `useRef` function in fetch-stats functionality in SellerDashboard component to create a smoothly animated value counter for displaying "Balance" and "Products" data. The logic takes advantage of the CountUp library, providing a more appealing and dynamic data presentation compared to static text. Adjustments were also made to the `plugins.bundle.js` to support the animated counter implementation.

--- src/components/customer/CustomerDashboard.js
+++ src/components/customer/CustomerDashboard.js
@@ -1,18 +1,34 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {PageTitle} from "../../_metronic/layout/core";
 import {KTIcon} from "../../_metronic/helpers";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 const DashboardPage = () => {
+    const countUpRef = useRef(null);
+    let countUpAnim;
+
     useEffect(() => {
-        const data = ["1 000$", '5']
+        const data = ['5']
         document.querySelectorAll(".fetch-stats")
             .forEach((el, index) => {
                 setTimeout(() => {
                     el.textContent = data[index];
                 }, 700)
             })
+
+        initCountUp();
+
     }, []);
+
+    async function initCountUp() {
+        countUpAnim = new window.CountUp(countUpRef.current, 100);
+        if (!countUpAnim.error) {
+            countUpAnim.start();
+        } else {
+            console.error(countUpAnim.error);
+        }
+    }
+
     return <>
         {/* begin::Row */}
         <div className='row g-5 g-xl-10 mb-5 mb-xl-10 justify-content-center'>
@@ -20,8 +36,8 @@ const DashboardPage = () => {
                 <a href="#" className="card bg-body hoverable card-xl-stretch mb-xl-8">
                     <div className="card-body p-5 text-center">
                         <KTIcon iconType="duotone" iconName="abstract-23" className="fs-3x"/>
-                        <div id="requests_count" className="fetch-stats text-gray-900 fw-bolder fs-2 mb-2 mt-5">
-                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                        <div ref={countUpRef} className=" text-gray-900 fw-bolder fs-2 mb-2 mt-5">
+                            0
                         </div>
                         <div className="fw-bold text-gray-400">Balance</div>
                     </div>
@@ -32,7 +48,7 @@ const DashboardPage = () => {
                 <a href="#" className="card bg-body hoverable card-xl-stretch mb-xl-8">
                     <div className="card-body p-5 text-center">
                         <KTIcon iconType="duotone" iconName="crown" className="fs-3x"/>
-                        <div id="requests_count" className="fetch-stats text-gray-900 fw-bolder fs-2 mb-2 mt-5">
+                        <div className="fetch-stats text-gray-900 fw-bolder fs-2 mb-2 mt-5">
                             <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
                         </div>
                         <div className="fw-bold text-gray-400">Bidding</div>
