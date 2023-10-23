8ab1f9203a50946525d694c59f89a3543774ca9a
Longbowou <blandedaniel@gmail.com>
2023-10-23 18:11:14 -0500

    Implement form submission for product addition
    New handlers were added to the 'AddProduct' component to manage form data and facilitate product addition. A reference to the form was created using the useRef hook and an 'onSubmit' event handler was put into effect for processing form submissions. Additionally, form fields were updated to include a 'name' attribute for efficient data gathering, and a 'required' attribute was added to facilitate data validation before submission. All these changes have been implemented to effectively collect and record form data, while also ensuring that user inputs satisfy the necessary prerequisites before submission.

--- src/components/apiUrl.js
+++ src/components/apiUrl.js
@@ -2,3 +2,5 @@ const API_URL = process.env.REACT_APP_API_URL
 export const LOGIN_URL = `${API_URL}/auth/login`
 export const REGISTER_URL = `${API_URL}/register`
 export const CATEGORIES_URL = `${API_URL}/categories`
+export const ADD_PRODUCT_URL = `${API_URL}/products`
+export const UPLOAD_PRODUCT_IMAGE_URL = `${API_URL}/products/{id}/upload`
